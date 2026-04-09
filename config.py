a67c43986e08f8b2ee789edee4671ba0bc9fb03d
Your Name <you@example.com>
2026-04-09 15:58:32 +0500

    updated groq api

--- config.py
+++ config.py
@@ -5,9 +5,5 @@ load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-<<<<<<< HEAD
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-=======
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
->>>>>>> 9c953332259e4d8a1d5e67dc770573031b2a93bb
 
